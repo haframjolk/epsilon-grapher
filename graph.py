b57d90a62d9f412d20d680c92fe91e518a74ef28
Reyn Alpha <reynalpha@outlook.com>
2021-03-31 14:58:56 +0000

    Create a horizontal bar graph

--- graph.py
+++ graph.py
@@ -2,7 +2,6 @@
 
 import json
 import matplotlib.pyplot as plt
-from matplotlib.ticker import MaxNLocator
 
 
 def init():
@@ -14,16 +13,34 @@ def init():
 
 def draw():
     """Draws a bar graph showing the results"""
-    fig = plt.figure(dpi=150)
-    bars = plt.bar(data.keys(), data.values())
-    # Make empty votes bar red
+    # Create figure
+    plt.figure(figsize=(6, 5), dpi=150)
+    # y range
+    y_pos = range(len(data))
+    # Plot results
+    bars = plt.barh(y_pos, data.values(), align="center")
+    # Hide ticks
+    plt.tick_params(axis="both", length=0)
+    # Set tick labels to candidate names
+    plt.yticks(y_pos, data.keys())
+    # Set empty ballot bar color to red
     bars[-1].set_color("r")
     # Set labels
+    plt.xlabel("Votes")
     plt.title("Election Results")
-    plt.xlabel("Candidates")
-    plt.ylabel("Votes")
-    # Show only integers on y-axis
-    fig.gca().yaxis.set_major_locator(MaxNLocator(integer=True))
+    # Write number of votes next to each bar
+    for rect, val in zip(bars.patches, data.values()):
+        plt.text(rect.get_width() + 0.25,
+                 rect.get_y() + rect.get_height() / 2,
+                 str(val),
+                 ha="left",
+                 va="center")
+    # Invert y axis
+    plt.gca().invert_yaxis()
+    # Set margins to fit vote labels
+    plt.margins(0.1, 0.05)
+    # Fit everything to the current layout
+    plt.tight_layout()
     # Show graph
     plt.show()
 
